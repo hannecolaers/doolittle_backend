77d002a300ad1c0ade4d88b469b2b8badb79960a
hannecolaers <hannecolaers@hotmail.com>
2017-12-08 10:59:59 +0100

    update .gitignore

--- unleashed-env-01/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\11501890\desktop\git-it\doolittle_backend\unleashed-env-01\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
