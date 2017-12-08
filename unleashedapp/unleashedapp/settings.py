9bc9df361b950082b7c65f582da79bcd117c2a18
Yannick <yannick.franssen@outlook.com>
2017-12-08 11:40:11 +0100

    Set Django to use the MySQL driver, add some README instructions

--- unleashedapp/unleashedapp/settings.py
+++ unleashedapp/unleashedapp/settings.py
@@ -74,9 +74,10 @@ WSGI_APPLICATION = 'unleashedapp.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'unleashed',
+        'USER': 'root'
     }
 }
 
