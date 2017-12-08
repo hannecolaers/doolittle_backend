7bb84d6a8d7f424629b3323fe12ac17f6ec4a988
Branco <branco.dortangs@student.pxl.be>
2017-12-08 11:47:59 +0200

    Added environment and Django.
    The environment is named unleashed-env-01 documentation is found on Confluence.

---
+++ unleashed-env-01/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\11501890\desktop\git-it\doolittle_backend\unleashed-env-01\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
