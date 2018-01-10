95d192fb4b04a71aab17440f92e2d00bbc9d7172
Yannick <yannick.franssen@outlook.com>
2018-01-10 14:48:29 +0100

    Update view to use SAC and OAuth2

--- unleashedapp/training/views.py
+++ unleashedapp/training/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 # Authenticate with Google
-gc = gspread.login('pxl.unleashed@gmail.com', 'unlea5hed')
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
 
 # Get the Training spreadsheet file
-sh = gc.open_by_key('1jEZR1uaEylQ05AohVvRpdQSWGOl7nDQE4oDtTWVAGkw')
-worksheet = sh.get_worksheet(0)
+sheet = client.open_by_key('1jEZR1uaEylQ05AohVvRpdQSWGOl7nDQE4oDtTWVAGkw').sheet1
 
+list_of_hashes = sheet.get_all_records()
