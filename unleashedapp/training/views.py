3d7c4a6d7c71d8c3cbd708a7f7c973c251b7db89
Yannick <yannick.franssen@outlook.com>
2018-01-10 15:09:42 +0100

    Make API work :D

--- unleashedapp/training/views.py
+++ unleashedapp/training/views.py
@@ -4,10 +4,11 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Authenticate with Google
 scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('C:/Python/backend/unleashedapp/training/client_secret.json', scope)
 client = gspread.authorize(creds)
 
 # Get the Training spreadsheet file
 sheet = client.open_by_key('1jEZR1uaEylQ05AohVvRpdQSWGOl7nDQE4oDtTWVAGkw').sheet1
 
 list_of_hashes = sheet.get_all_records()
+print(list_of_hashes)
