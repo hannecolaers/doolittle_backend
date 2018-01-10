b43a9aaf33def728ed366846d4164e174cfc1825
Yannick <yannick.franssen@outlook.com>
2018-01-10 13:24:10 +0100

    Add Gspread login and get dummy spreadsheet

--- unleashedapp/training/views.py
+++ unleashedapp/training/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import gspread
 
-# Create your views here.
+# Authenticate with Google
+gc = gspread.login('pxl.unleashed@gmail.com', 'unlea5hed')
+
+# Get the Training spreadsheet file
+sh = gc.open_by_key('1jEZR1uaEylQ05AohVvRpdQSWGOl7nDQE4oDtTWVAGkw')
+worksheet = sh.get_worksheet(0)
+
