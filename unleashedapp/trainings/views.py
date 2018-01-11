a84b251a7e1868949a1c34ffbb400081e699c64d
Yannick <yannick.franssen@outlook.com>
2018-01-11 16:23:07 +0100

    Move client_secret.json one level up to make it universal

--- unleashedapp/trainings/views.py
+++ unleashedapp/trainings/views.py
@@ -8,8 +8,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Authenticate with Google
 scope = ['https://spreadsheets.google.com/feeds']
-## TODO: This can't be an absolute path, won't work for anybody else with a different path
-creds = ServiceAccountCredentials.from_json_keyfile_name('C:/Python/backend/unleashedapp/trainings/client_secret.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
 # Get the Training spreadsheet file
