d5d034d3b12e7da921f1bafbccd3bd628ce2fb8a
Yannick <yannick.franssen@outlook.com>
2018-01-18 09:50:18 +0100

    Drop serializer

--- unleashedapp/trainings/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers, status
-from rest_framework.exceptions import APIException
-
-class TrainingSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    #serialized_obj = serializers.serialize('json', [ obj, ])
