a884a4685e087031b638fb91a714a41c141d7da8
Yannick <yannick.franssen@outlook.com>
2018-01-18 13:34:28 +0100

    Clean-up code with a number of enhancements
    - Put often returning code in its own function
    - Put long if-statements in their own function
    - Rewrite 2 lines to one
    - Use comments only when required
    - Make comments contain only useful info
    - Return empty arrays instead of "[]" when nothing found
    - Make code more readable
    - Put positive result of if statements first

--- unleashedapp/trainings/views.py
+++ unleashedapp/trainings/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse, HttpRequest
-from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 from rest_framework.views import APIView
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from rest_framework import status
 import gspread
-import logging
 import json
+import logging
 from oauth2client.service_account import ServiceAccountCredentials
 
 # Authenticate with Google
@@ -15,81 +11,89 @@ scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
-logger = logging.getLogger(__name__)
-
 # Get the Training spreadsheet file
 spreadsheet = client.open_by_key('1jEZR1uaEylQ05AohVvRpdQSWGOl7nDQE4oDtTWVAGkw')
 
+logger = logging.getLogger(__name__)
+
+def generate_json(row):
+    return {
+        "date": row[0].value,
+        "days": row[1].value,
+        "firstname": row[2].value,
+        "lastname": row[3].value,
+        "team": row[4].value,
+        "training": row[5].value,
+        "company": row[6].value,
+        "city": row[7].value,
+        "cost": row[8].value,
+        "invoice": row[9].value,
+        "info": row[10].value
+    }
+
+def get_sheet(sheet):
+    return spreadsheet.worksheet(sheet)
+
+def validate_data_has_all(data):
+    return True if "date" in data and "days" in data and "firstname" in data and "lastname" in data and "team" in data and "training" in data and "company" in data and "city" in data and "cost" in data and "invoice" in data and "info" in data else False
+
+def validate_data_has_one(data):
+    return True if "date" in data or "days" in data or "firstname" in data or "lastname" in data or "team" in data or "training" in data or "company" in data or "city" in data or "cost" in data or "invoice" in data or "info" in data else False
+
 
 class TrainingList(APIView):
-    def get(self, request, format=None):
-        sheet = request.GET.get('sheet', 'Data')
-        worksheet = spreadsheet.worksheet(sheet)
+    def get(self, request):
+        worksheet = get_sheet(request.GET.get('sheet', 'Data'))
         list_of_records = worksheet.get_all_records()
         if list_of_records == "":
             return JsonResponse([], safe=False, status=status.HTTP_200_OK)
         else:
             return JsonResponse(list_of_records, safe=False, status=status.HTTP_200_OK)
 
-    def post(self, request, format=None):
-        sheet = request.GET.get('sheet', 'Data')
-        worksheet = spreadsheet.worksheet(sheet)
+    def post(self, request):
+        worksheet = get_sheet(request.GET.get('sheet', 'Data'))
         data = json.loads(request.body.decode('utf-8'))
-        if "date" in data and "days" in data and "firstname" in data and "lastname" in data and "team" in data and "training" in data and "company" in data and "city" in data and "cost" in data and "invoice" in data and "info" in data:
+        if validate_data_has_all(data):
             worksheet.append_row([data["date"], data["days"], data["firstname"], data["lastname"], data["team"], data["training"], data["company"], data["city"], data["cost"], data["invoice"], data["info"]])
             return JsonResponse(data, safe=False, status=status.HTTP_201_CREATED)
         else:
-            return JsonResponse("[]", safe=False, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse([], safe=False, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TrainingDetail(APIView):
-    def get(self, request, id, format=None):
+    def get(self, request, id):
         """
         GET a single training
         """
-        sheet = request.GET.get('sheet', 'Data')
-        worksheet = spreadsheet.worksheet(sheet)
-        if id == "1" or worksheet.row_count < int(id):
-            # In this context, id 1 is the row of headings, this shouldn't be gettable
-            return JsonResponse("[]", safe=False, status=status.HTTP_404_NOT_FOUND)
+        worksheet = get_sheet(request.GET.get('sheet', 'Data'))
+        if id == "1" or worksheet.row_count < int(id): # 1 is the row of headings
+            return JsonResponse([], safe=False, status=status.HTTP_404_NOT_FOUND)
         else:
             result = []
-            row_value = worksheet.range(id, 1, id, 11)
-            if row_value[0].value != "":
-                result_json = {
-                    "date": row_value[0].value,
-                    "days": row_value[1].value,
-                    "firstname": row_value[2].value,
-                    "lastname": row_value[3].value,
-                    "team": row_value[4].value,
-                    "training": row_value[5].value,
-                    "company": row_value[6].value,
-                    "city": row_value[7].value,
-                    "cost": row_value[8].value,
-                    "invoice": row_value[9].value,
-                    "info": row_value[10].value,
-                }
+            row_value = worksheet.range(int(id), 1, int(id), 11)
+            if row_value[0].value == "":
+                return JsonResponse([], safe=False, status=status.HTTP_404_NOT_FOUND)
+            else:
+                result_json = generate_json(row_value)
                 result.append(result_json)
                 return JsonResponse(result, safe=False)
-            else:
-                return JsonResponse("[]", safe=False, status=status.HTTP_404_NOT_FOUND)
 
-    def put(self, request, id, format=None):
+    def put(self, request, id):
         """
         PUT a row into the spreadsheet
         """
-        sheet = request.GET.get('sheet', 'Data')
-        worksheet = spreadsheet.worksheet(sheet)
-        if id == "1" or worksheet.row_count < int(id):
-            # In this context, id 1 is the row of headings, this shouldn't be overwritten
-            return JsonResponse("[]", safe=False, status=status.HTTP_404_NOT_FOUND)
+        worksheet = get_sheet(request.GET.get('sheet', 'Data'))
+        if id == "1" or worksheet.row_count < int(id): # 1 is the row of headings
+            return JsonResponse([], safe=False, status=status.HTTP_404_NOT_FOUND)
         else:
             data = json.loads(request.body.decode('utf-8'))
-            # Get the data from the current row
             result = []
+            # Get the data from the current row
             row_value = worksheet.range(id, 1, id, 11)
-            if row_value[0].value != "":
-                if "date" in data or "days" in data or "firstname" in data or "lastname" in data or "team" in data or "training" in data or "company" in data or "city" in data or "cost" in data or "invoice" in data or "info" in data:
+            if row_value[0].value == "":
+                return JsonResponse([], safe=False, status=status.HTTP_404_NOT_FOUND)
+            else:
+                if validate_data_has_one(data):
                     if "date" in data:
                         worksheet.update_cell(id, 1, data['date'])
                     if "days" in data:
@@ -114,67 +118,38 @@ class TrainingDetail(APIView):
                         worksheet.update_cell(id, 11, data['info'])
                     # Request the updated row
                     row_value = worksheet.range(id, 1, id, 11)
-                    result_json = {
-                        "date": row_value[0].value,
-                        "days": row_value[1].value,
-                        "firstname": row_value[2].value,
-                        "lastname": row_value[3].value,
-                        "team": row_value[4].value,
-                        "training": row_value[5].value,
-                        "company": row_value[6].value,
-                        "city": row_value[7].value,
-                        "cost": row_value[8].value,
-                        "invoice": row_value[9].value,
-                        "info": row_value[10].value,
-                    }
+                    result_json = generate_json(row_value)
                     result.append(result_json)
-                    return JsonResponse(result, safe=False, status=status.HTTP_201_CREATED)
+                    return JsonResponse(result, safe=False, status=status.HTTP_200_OK)
                 else:
-                    return JsonResponse("[]", safe=False, status=status.HTTP_400_BAD_REQUEST)
-            else:
-                return JsonResponse("[]", safe=False, status=status.HTTP_404_NOT_FOUND)
+                    return JsonResponse([], safe=False, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id):
         """
         DELETE a row from the spreadsheet
         """
-        sheet = request.GET.get('sheet', 'Data')
-        worksheet = spreadsheet.worksheet(sheet)
-        if id == "1" or worksheet.row_count < int(id):
-            # In this context, id 1 is the row of headings, this shouldn't be deleteable
-            return JsonResponse("[]", safe=False, status=status.HTTP_404_NOT_FOUND)
+        worksheet = get_sheet(request.GET.get('sheet', 'Data'))
+        if id == "1" or worksheet.row_count < int(id): # 1 is the row of headings
+            return JsonResponse([], safe=False, status=status.HTTP_404_NOT_FOUND)
         else:
             worksheet.delete_row(int(id))
-            return JsonResponse("[]", safe=False, status=status.HTTP_200_OK)
+            return JsonResponse([], safe=False, status=status.HTTP_204_NO_CONTENT)
 
 
 class TrainingAllDetail(APIView):
-    def get(self, request, firstname, lastname, format=None):
+    def get(self, request, firstname, lastname):
         """
         List all trainings from one person
         """
-        sheet = request.GET.get('sheet', 'Data')
-        worksheet = spreadsheet.worksheet(sheet)
+        worksheet = get_sheet(request.GET.get('sheet', 'Data'))
         cell_list = worksheet.findall(firstname)
         data = []
-        if cell_list != []:
+        if cell_list == []:
+            return JsonResponse([], safe=False, status=status.HTTP_404_NOT_FOUND)
+        else:
             for cell in cell_list:
                 row_value = worksheet.range(cell.row, 1, cell.row, 11)
                 if row_value[2].value == firstname and row_value[3].value == lastname:
-                    result_json = {
-                        "date": row_value[0].value,
-                        "days": row_value[1].value,
-                        "firstname": row_value[2].value,
-                        "lastname": row_value[3].value,
-                        "team": row_value[4].value,
-                        "training": row_value[5].value,
-                        "company": row_value[6].value,
-                        "city": row_value[7].value,
-                        "cost": row_value[8].value,
-                        "invoice": row_value[9].value,
-                        "info": row_value[10].value,
-                    }
+                    result_json = generate_json(row_value)
                     data.append(result_json)
             return JsonResponse(data, safe=False, status=status.HTTP_200_OK)
-        else:
-            return JsonResponse([], safe=False, status=status.HTTP_404_NOT_FOUND)
