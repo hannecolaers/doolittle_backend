9ef40c9d63ec81db8b272263b11a89c42285157e
Yannick <yannick.franssen@outlook.com>
2018-01-18 16:06:53 +0100

    Test clean-up

--- unleashedapp/trainings/tests.py
+++ unleashedapp/trainings/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase, Client
 from rest_framework import status
 import json
-
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -14,7 +13,8 @@ class SquadTestCase(TestCase):
         # Create a new, empty spreadsheet and worksheet
         self.sh = self.gclient.open_by_key('1jEZR1uaEylQ05AohVvRpdQSWGOl7nDQE4oDtTWVAGkw')
         self.ws = self.sh.worksheet('TestSheet')
-        self.ws.resize(rows=1, cols=11)
+        self.ws.resize(rows=2, cols=11)
+        self.ws.insert_row(["1/1/2018",1,"Testuser","Testname","Care","Text","PXL","Hasselt",1.8,"Yep","Nope"], 2)
         self.training_json = {
             "date": "1/1/2018",
             "days": 1,
@@ -29,8 +29,8 @@ class SquadTestCase(TestCase):
             "info": "Nope"
         }
         self.training_json_partial = {
-            "date": "1/1/2018",
-            "days": 1,
+            "date": "10/1/2018",
+            "days": 10,
             "firstname": "Testuser",
             "lastname": "Franssen",
             "team": "Careful",
@@ -40,20 +40,19 @@ class SquadTestCase(TestCase):
     """
     Tests for the /trainings/<id>/ path
     """
-    '''     def test_get_training_returns_200_when_found(self):
+    def test_get_training_returns_200_when_found(self):
         """
         A GET request on /trainings/<id>/ should return an array of trainings
         """
-        self.ws.append_row(["1/1/2018",1,"Testuser","Testname","Care","Text","PXL","Hasselt",1.8,"Yep","Nope"])
         response = self.client.get('/trainings/2/?sheet=TestSheet')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-    '''
-    def test_get_training_returns_404_when_requesting_1(self):
+        
+    def test_get_training_returns_400_when_requesting_1(self):
         """
         A GET request on /trainings/<id>/ should return an array of trainings
         """
         response = self.client.get('/trainings/1/?sheet=TestSheet')
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         
     def test_get_training_returns_404_when_not_found(self):
         """
@@ -73,23 +72,20 @@ class SquadTestCase(TestCase):
         """
         A GET request on /trainings/<id>/ should return an array of trainings
         """
-        self.ws.append_row(["1/1/2018",1,"Testuser","Testname","Care","Text","PXL","Hasselt",1.8,"Yep","Nope"])
         response = self.client.put('/trainings/2/?sheet=TestSheet', json.dumps(self.training_json_partial), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
-    def test_put_training_returns_404_when_requesting_1(self):
+    def test_put_training_returns_400_when_requesting_1(self):
         """
         A GET request on /trainings/<id>/ should return an array of trainings
         """
-        self.ws.append_row(["1/1/2018",1,"Testuser","Testname","Care","Text","PXL","Hasselt",1.8,"Yep","Nope"])
         response = self.client.put('/trainings/1/?sheet=TestSheet', json.dumps(self.training_json_partial), content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_put_training_returns_404_when_not_found(self):
         """
         A GET request on /trainings/<id>/ should return an array of trainings
         """
-        self.ws.append_row(["1/1/2018",1,"Testuser","Testname","Care","Text","PXL","Hasselt",1.8,"Yep","Nope"])
         response = self.client.put('/trainings/999/?sheet=TestSheet', json.dumps(self.training_json_partial), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
@@ -97,7 +93,6 @@ class SquadTestCase(TestCase):
         """
         A POST request on /trainings/<id>/ using the wrong format should return a 400 error
         """
-        self.ws.append_row(["1/1/2018",1,"Testuser","Testname","Care","Text","PXL","Hasselt",1.8,"Yep","Nope"])
         data = {'title': 'Newtraining'}
         response = self.client.put('/trainings/2/?sheet=TestSheet', json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -106,7 +101,6 @@ class SquadTestCase(TestCase):
         """
         A DELETE request on /trainings/<id>/ should delete a training
         """
-        self.ws.append_row(["1/1/2018",1,"Testuser","Testname","Care","Text","PXL","Hasselt",1.8,"Yep","Nope"])
         response = self.client.delete('/trainings/2/?sheet=TestSheet')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         
@@ -124,7 +118,6 @@ class SquadTestCase(TestCase):
         """
         A GET request on /trainings/ should return an array of trainings
         """
-        self.ws.append_row(["1/1/2018",1,"Testuser","Testname","Care","Text","PXL","Hasselt",1.8,"Yep","Nope"])
         response = self.client.get('/trainings/?sheet=TestSheet')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -132,6 +125,7 @@ class SquadTestCase(TestCase):
         """
         A GET request on /trainings/ should still work with no results and return an empty array
         """
+        self.ws.resize(rows=1, cols=11)
         response = self.client.get('/trainings/?sheet=TestSheet')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -178,16 +172,16 @@ class SquadTestCase(TestCase):
         """
         A GET request on /trainings/<firstname>/<lastname>/ should return an array of trainings
         """
-        self.ws.append_row(["1/1/2018",1,"Testuser","Testname","Care","Text","PXL","Hasselt",1.8,"Yep","Nope"])
         response = self.client.get('/trainings/Testuser/Testname/?sheet=TestSheet')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
-    def test_get_training_from_employee_returns_404_when_not_found(self):
+    def test_get_training_from_employee_returns_200_when_non_found(self):
         """
         A GET request on /trainings/<firstname>/<lastname>/ should return an array of trainings
         """
+        self.ws.resize(rows=1, cols=11)
         response = self.client.get('/trainings/Testuser/Testname/?sheet=TestSheet')
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_post_training_from_employee_returns_405(self):
         """
