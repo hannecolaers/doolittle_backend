a8bab8b01683f480cfc648457ee3096927f6211d
Yannick <yannick.franssen@outlook.com>
2018-02-09 09:05:56 +0100

    Develop, but with working training

--- unleashedapp/unleashedapp/settings.py
+++ unleashedapp/unleashedapp/settings.py
@@ -50,23 +50,6 @@ INSTALLED_APPS = [
 # While developing allow every host
 ALLOWED_HOSTS = ['*']
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
-        'rest_framework_social_oauth2.authentication.SocialAuthentication',
-    ),
-}
-
-AUTHENTICATION_BACKENDS = (
-    'social_core.backends.google.GoogleOAuth2',
-    'rest_framework_social_oauth2.backends.DjangoOAuth2',
-    'django.contrib.auth.backends.ModelBackend',
-)
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '213697302597-ncg6a9elrdc97k4r4ra5ckql07dpehvm.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'bS9t-LK4hh3u43X8e7dLFmWu'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['']
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
